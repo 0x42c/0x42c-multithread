8eea4936aba1b18644fdf99e9109c8ac8e8297b5
Drew DeVault <sir@cmpwn.com>
2012-04-25 14:29:28 -0600

    Improved scripts and fixed memory bug

--- build/build.bat
+++ build/build.bat
@@ -1,3 +1,5 @@
 @echo off
+IF EXIST ..\bin GOTO SKIPBINDIR
 mkdir ..\bin
+:SKIPBINDIR
 orgASM.exe ../src/kernel/base.dcpu --output-file ../bin/kernel.bin --listing ../bin/kernel.lst --include ../include --working-directory ../src/kernel
--- build/stats.sh
+++ build/stats.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-mkdir ../bin
+if [ -d "../bin" ]; then
+    mkdir ../bin
+fi
 mono orgASM.exe ../src/kernel/base.dcpu --output-file ../bin/kernel.bin --listing ../bin/kernel.lst --include ../include --working-directory ../src/kernel --equate ENABLE_STATS 1
