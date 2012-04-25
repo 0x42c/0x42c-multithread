345b6528ef6c4ede622493adfc2cac45476c7a6e
Drew DeVault <sir@cmpwn.com>
2012-04-25 16:06:10 -0600

    fixed stats scripts

--- build/stats.bat
+++ build/stats.bat
@@ -2,4 +2,4 @@
 IF EXIST ..\bin GOTO SKIPBINDIR
 mkdir ..\bin
 :SKIPBINDIR
-orgASM.exe ../src/kernel/base.dcpu --output-file ../bin/kernel.bin --listing ../bin/kernel.lst --include ../include --working-directory ../src/kernel --equate ENABLE_STATS 1
+orgASM.exe ../src/kernel/base.dasm --output-file ../bin/kernel.bin --listing ../bin/kernel.lst --include ../include --working-directory ../src/kernel --equate ENABLE_STATS 1
