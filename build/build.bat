cca8ee18eaf2fb3427cd62678ba75062db021a9c
Drew DeVault <sir@cmpwn.com>
2012-04-25 14:07:36 -0600

    Disabled stats by default, disabled debug mode on .orgASM

--- build/build.bat
+++ build/build.bat
@@ -1,3 +1,3 @@
 @echo off
 mkdir ..\bin
-orgASM.exe ../src/kernel/base.dcpu --output-file ../bin/kernel.bin --listing ../bin/kernel.lst --include ../include --working-directory ../src/kernel --debug-mode
+orgASM.exe ../src/kernel/base.dcpu --output-file ../bin/kernel.bin --listing ../bin/kernel.lst --include ../include --working-directory ../src/kernel
