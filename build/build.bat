5437922868cdc0151c6f1184d51b2584f41691dd
Drew DeVault <sir@cmpwn.com>
2012-04-25 11:38:51 -0600

    Fixed kernel.inc

--- build/build.bat
+++ build/build.bat
@@ -1,3 +1,3 @@
 @echo off
 mkdir ..\bin
-orgASM.exe ../src/kernel/base.dcpu --output-file ../bin/kernel.bin --listing ../bin/kernel.lst --include ../src/include --working-directory ../src/kernel
+orgASM.exe ../src/kernel/base.dcpu --output-file ../bin/kernel.bin --listing ../bin/kernel.lst --include ../include --working-directory ../src/kernel
--- build/build.sh
+++ build/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 mkdir ../bin
-mono orgASM.exe ../src/kernel/base.dcpu --output-file ../bin/kernel.bin --listing ../bin/kernel.lst --include ../src/include --working-directory ../src/kernel
+mono orgASM.exe ../src/kernel/base.dcpu --output-file ../bin/kernel.bin --listing ../bin/kernel.lst --include ../include --working-directory ../src/kernel
