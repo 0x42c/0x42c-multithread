4b711b646afb76862e98ab45c84978242f545dfd
Drew DeVault <sir@cmpwn.com>
2012-04-25 16:26:55 -0600

    Set up stubs for loading bootloader

--- build/stats.sh
+++ build/stats.sh
@@ -2,4 +2,5 @@
 if [ -d "../bin" ]; then
     mkdir ../bin
 fi
+echo ====Building kernel...====
 mono orgASM.exe ../src/kernel/base.dasm --output-file ../bin/kernel.bin --listing ../bin/kernel.lst --include ../include --working-directory ../src/kernel --equate ENABLE_STATS 1
