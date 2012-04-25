23b43b6cad38575604b81ed806bb6ec2f6684175
Drew DeVault <sir@cmpwn.com>
2012-04-25 16:05:35 -0600

    Added support for Generic Clock

--- build/build.bat
+++ build/build.bat
@@ -2,5 +2,4 @@
 IF EXIST ..\bin GOTO SKIPBINDIR
 mkdir ..\bin
 :SKIPBINDIR
-orgASM.exe ../src/kernel/base.dasm --output-file ../bin/kernel.bin --listing ../bin/kernel.lst 
---include ../include --working-directory ../src/kernel
+orgASM.exe ../src/kernel/base.dasm --output-file ../bin/kernel.bin --listing ../bin/kernel.lst --include ../include --working-directory ../src/kernel
