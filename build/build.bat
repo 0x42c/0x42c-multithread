b33c7c765de7aa135ccba745a3079d7dfc282ce2
Drew DeVault <sir@cmpwn.com>
2012-04-25 00:30:02 -0600

    Initial commit with skeleton project

---
+++ build/build.bat
@@ -0,0 +1,2 @@
+@echo off
+orgASM.exe ../src/kernel/base.dcpu --output-file ../bin/kernel.bin --listing ../bin/kernel.lst --include ../src/include --working-directory ../src/kernel --debug-mode
---
+++ build/build.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+mono orgASM.exe ../src/kernel/base.dcpu --output-file ../bin/kernel.bin --listing ../bin/kernel.lst --include ../src/include --working-directory ../src/kernel
